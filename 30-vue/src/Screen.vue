b09633a1e1c498738c7ed22fc59bd1f98debb5a4
Jonas Bandi <blog@jonasbandi.net>
2023-03-12 14:05:47 +0100

    Vue Demo

--- 30-vue/src/Screen.vue
+++ 30-vue/src/Screen.vue
@@ -1,10 +1,14 @@
 <template>
-  <h3>Vue Demo</h3>
+  <h1>{{ title }}</h1>
+  <!--  <input v-model="title" />-->
   <Counter />
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import Counter from "./Counter.vue";
+
+const title = ref("Vue Demo");
 </script>
 
 <style scoped></style>
